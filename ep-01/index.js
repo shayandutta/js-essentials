e2fa00ac80e1f872c1003d083f72becb3631ecfc
shayandutta <shayandutta23@gmail.com>
2025-02-17 10:19:40 +0530

    ep-01

--- ep-01/index.js
+++ ep-01/index.js
@@ -8,3 +8,10 @@ const fullName = "shayan";
 console.log("Hello Mr. " +fullName+ " your age is: "+age); //string concatenation
 
 console.log(`Hello Mr. ${fullName} your age is: ${age}`) //template literal
+
+//Q. print sum of two numbers
+
+const a = 10;
+const b = 20;
+
+console.log(`sum of ${a} and ${b} is: ${a+b}`);
