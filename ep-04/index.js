adfe206da36324d33577f1fc4699ae93ed9ddbef
shayandutta <shayandutta23@gmail.com>
2025-02-17 10:50:17 +0530

    ep-04: let and const

---
+++ ep-04/index.js
@@ -0,0 +1,11 @@
+// const a = 10; //constant variable, cannot further modify
+// a=20;
+// console.log(a);
+
+let b=100; //let variable can be further modified
+b=200;
+console.log(b);
+
+var x =20; //can also be modified but we will be using CONST and LET
+x=40;
+console.log(x);
